8ce1dc52bfa790aec02210394659a7997c4b0a63
sergiylontkovsky <ls1>
2018-01-30 15:29:03 +0200

    first

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+
+  data() {
+  return{
+    currencyPrice: {
+
+    }
+  }
+  },
+
+  components: {
+    HelloWorld
+  },
+
+  methods: {
+    getCurrencyPrice: function() {
+      this.$http.get('/https://api.coindesk.com/v1/bpi/currentprice.json', {
+
+      })
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <div id="currencyPrice">
+    BTC/USD: {{price}}
+    </div>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+
+
+export default {
+  name: 'hello',
+  data: () => ({
+    cryptoPrice: "",
+    price: ""
+  }),
+
+  created: function () {
+    axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => {
+        this.cryptoPrice = response.data
+        this.price = response.data.bpi.USD.rate
+
+      })
+
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
