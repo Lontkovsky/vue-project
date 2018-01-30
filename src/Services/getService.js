bc20eeab856bb7498393d2d4bbac0e5d7d0d05cb
sergiylontkovsky <ls1>
2018-01-30 18:04:16 +0200

    trying to add service

--- src/Services/getService.js
+++ src/Services/getService.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import axios from 'axios'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource);
 
 export default new Vue ({
 
@@ -7,10 +10,9 @@ export default new Vue ({
     price: ""
   }),
 
-  beforeCreate: {
-    async function () {
-      const json = await axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-      this.price = json.data.bpi.USD.rate
-    }
+  created: async function () {
+    const json = await axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    this.price = json.data.bpi.USD.rate
   }
 })
+
